c881627a0ec95b10cabad84cc879cc2dcfef90b6
Sergio03 <mariano13martin@gmail.com>
2023-04-02 17:17:08 -0300

    Punto 3 modificado, Punto 5

--- tp3_3.c
+++ tp3_3.c
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define FILAS 5
-#define COLUMNAS 10
+#define COLUMNAS 15
 
 int main(){
-    char *V[5];
+    int cant;
+    char arreglo[COLUMNAS];
 
-    printf("Ingrese 5 Nombres:\n");
-    for (int i = 0; i < FILAS; i++)
+    printf("Ingrese la cantidad de nombres a cargar: ");
+    fflush(stdin);
+    scanf("%d",&cant);
+
+    char **V = (char **)malloc(sizeof(char *) * cant);
+
+    printf("Ingrese los Nombres:\n");
+    for (int i = 0; i < cant; i++)
     {
-        V[i] = (char *)malloc(sizeof(char) * COLUMNAS);
-        gets(V[i]);
+        fflush(stdin);
+        gets(arreglo);
+        V[i] = (char *)malloc(sizeof(char) * (strlen(arreglo)+1));
+        strcpy(V[i],arreglo);
     }
 
     printf("-------Nombres:-------\n");
-    for (int i = 0; i < FILAS; i++)
+    for (int i = 0; i < cant; i++)
     {
         printf("%s\n",V[i]);
     }
     
-    for (int i = 0; i < FILAS; i++)
+    for (int i = 0; i < cant; i++)
     {
         free(V[i]);
     }
+    free(V);
     return 0;
 }
